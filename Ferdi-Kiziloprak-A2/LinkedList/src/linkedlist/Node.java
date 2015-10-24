0763cd5d7503b7e32390223e85c5890c274ee839
Ferdi kızıltoprak <FerdikT@gmail.com>
2015-10-24 20:36:22 +0300

    Assignment2 Added

---
+++ Ferdi-Kiziloprak-A2/LinkedList/src/linkedlist/Node.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package linkedlist;
+
+/**
+ *
+ * @author FerdiKT
+ */
+public class Node<T> {
+    T data;
+    Node next;
+    
+    Node(T data){
+        this.data = data;
+    }
+}
